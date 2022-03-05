c63b44e5397173e77472c6cf355cecb20939059a
rohitburman <100778333+rohitburman@users.noreply.github.com>
2022-03-05 13:14:34 +0530

    Update Age_specific_Population.sql

--- Age_specific_Population.sql
+++ Age_specific_Population.sql
@@ -1,4 +1,4 @@
-What is the population proportion according to International population data on the basis of country code?
+--What is the population proportion according to International population data on the basis of country code?
 
 SELECT population FROM `bigquery-public-data.census_bureau_international.midyear_population_agespecific` AS age_specific
  RIGHT JOIN `bigquery-public-data.census_bureau_international.midyear_population` AS mid_year
