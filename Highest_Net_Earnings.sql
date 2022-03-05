a6fe2ac06b3c704a90cf60cfa81ad06012de9351
rohitburman <100778333+rohitburman@users.noreply.github.com>
2022-03-05 13:15:24 +0530

    Update Highest_Net_Earnings.sql

--- Highest_Net_Earnings.sql
+++ Highest_Net_Earnings.sql
@@ -1,4 +1,4 @@
-What is the highest net earnings as per the residence?
+--What is the highest net earnings as per the residence?
 
 SELECT MAX(Net_earnings_by_place_of_residence) AS Highest_Earning 
 FROM `bigquery-public-data.sdoh_bea_cainc30.fips`
