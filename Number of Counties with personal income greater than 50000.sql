8ab286279af6c86674c810d2e83f50f297149f67
rohitburman <100778333+rohitburman@users.noreply.github.com>
2022-03-05 12:34:27 +0530

    Update and rename Number of Counties with personal income greater than 50000 to Number of Counties with personal income greater than 50000.sql

--- Number of Counties with personal income greater than 50000.sql
+++ Number of Counties with personal income greater than 50000.sql
@@ -1,4 +1,4 @@
-What is the number of counties where the percapita personal income or personal income greater than or equal to 50000?
+--What is the number of counties where the percapita personal income or personal income greater than or equal to 50000?
 
 SELECT COUNT(GeoName) AS Geo_name 
 FROM `bigquery-public-data.sdoh_bea_cainc30.fips` WHERE Percapita_personal_income	>=50000
