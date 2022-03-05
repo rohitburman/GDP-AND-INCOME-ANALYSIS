5049a3fd98d7f6ecd45ffea3b21f2ac9709b3e59
rohitburman <100778333+rohitburman@users.noreply.github.com>
2022-03-05 13:14:59 +0530

    Update Employer contribution to pension.sql

--- Employer contribution to pension.sql
+++ Employer contribution to pension.sql
@@ -1,4 +1,5 @@
-Which are the counties where Employer's contribution to pension and insurance is greater than 7000?
+--Which are the counties where Employer's contribution to pension and insurance is greater than 7000?
+
 SELECT GeoName 
 FROM `bigquery-public-data.sdoh_bea_cainc30.fips` 
 WHERE Employer_contrib_pension_and_insurance > 7000 LIMIT 10
