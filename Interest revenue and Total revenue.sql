671cddcc2062525e497105b0b78cbcedf51dba7d
rohitburman <100778333+rohitburman@users.noreply.github.com>
2022-03-05 13:15:55 +0530

    Update Interest revenue and Total revenue.sql

--- Interest revenue and Total revenue.sql
+++ Interest revenue and Total revenue.sql
@@ -1,4 +1,4 @@
-What are the Employer Identification Numbers whose interest revenue is greater than 1000 and total revenue is between 1,50,000 and 2,50,000?
+--What are the Employer Identification Numbers whose interest revenue is greater than 1000 and total revenue is between 1,50,000 and 2,50,000?
 
 SELECT ein
 FROM `bigquery-public-data.irs_990.irs_990_pf_2015`
